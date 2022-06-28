fff77d0f4d7ad051e928c9d0bf14613dd17d30c6
deftonechris <deftonechris@msn.com>
2022-06-28 15:02:41 -0600

    added class and questions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { prompt } = require('inquirer');
-const questions = require('./questions');
-const html = require('./generateHTML');
-// prompts questions
-prompt(questions).then(markDown);
+import inquirer from 'inquirer';
+import questions from './questions.js';
+import Manager from './lib/Manager.js';
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
 
-const employees = [];
+const positions = {Managers:[],Engineers:[],Interns:[]};
+
+inquirer.prompt(questions).then();
---
+++ questions.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    type: 'confirm',
+    name: 'isEmpNeeded',
+    message: 'Would you like to fill a position?'
+  },
+  {
+    type:'list',
+    name: 'role',
+    message: 'Which position would you like fill?',
+    choices: ['Manager', 'Engineer', 'Intern'],
+    when: ({ isEmpNeeded }) => isEmpNeeded
+  },
+  {
+    type: 'input',
+    name: 'empName',
+    message: "What is the employee's name?",
+    when: ({ isEmpNeeded }) => isEmpNeeded
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the employee's ID?",
+    when: ({ isEmpNeeded }) => isEmpNeeded
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the employee's email?",
+    when: ({ isEmpNeeded }) => isEmpNeeded
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is the engineer's GitHub username?",
+    when: ({ role }) => role == 'Engineer'
+  },
+  {
+    type: 'input',
+    name: 'internSchool',
+    message: 'What school does the intern attend?',
+    when: ({ role }) => role == 'Intern'
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is the office number?',
+    when: ({ role }) => role == 'Manager'
+  }
+];
+
+export default questions;
